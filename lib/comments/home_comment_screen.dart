bd10eafeab03f117162d31ea7460d1cc12346ccb
technoenthus <malhotradisha2710@gmail.com>
2025-02-13 12:03:28 +0530

    why

--- lib/comments/home_comment_screen.dart
+++ lib/comments/home_comment_screen.dart
@@ -1,14 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:sliding_up_panel/sliding_up_panel.dart';
+//import 'package:sliding_up_panel/sliding_up_panel.dart';
 
 class CommentSection extends StatefulWidget {
   // post id logic addition
   final String userId;
   final String? username;
-  
-
 
   const CommentSection({
     Key? key,
@@ -22,7 +20,6 @@ class CommentSection extends StatefulWidget {
 
 class _CommentSectionState extends State<CommentSection> {
   final TextEditingController _commentController = TextEditingController();
-  
 
   // Function to add comment to Firestore
   Future<void> _addComment() async {
